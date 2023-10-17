6b31227cd4f8d06d514fe22e08bcbf2efef90a0a
hamomgs <hamomgomer947@gmail.com>
2023-10-16 21:15:26 -0300

    fix: server port dotenv
    Co-authored-by: Gabriel Apolinário <gabrielfelipealmeida@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const routes = require('./routes');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
